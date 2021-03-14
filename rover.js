c1ef80b41b22f23ebeb3b3518a8e272961045da9
Allison Logsdon-Hull <aslogsdon96@gmail.com>
2021-03-14 20:46:47 +0000

    first submission of rover assignment

--- rover.js
+++ rover.js
@@ -1,5 +1,89 @@
 class Rover {
-   // Write code here!
-}
+  constructor(position) {
+     this.completed = true;
+     this.position = position;
+     this.mode = 'NORMAL';
+     this.generatorWatts = 110;
+   }
+
+   receiveMessage(messageObj) {
+     let modeChange = {};
+     let moveChange = {};
+     let response = {
+       message: messageObj.name,
+       results: []
+     };
+    
+     let commandBlockRaw = messageObj.commands;
+     let commandBlock;
+
+     if (commandBlockRaw.length === undefined) {
+       commandBlock = [];
+       commandBlock.push(commandBlockRaw);
+     } else {
+       commandBlock = commandBlockRaw;
+     }
+
+     for (let b = 0; b < commandBlock.length; b++) {
+       let commandBlockTest = commandBlock[b];
+        for (let k in commandBlockTest) {
+          if (commandBlockTest[k] === 'STATUS_CHECK') {
+            let roverStatus = {
+            mode: this.mode,
+            generatorWatts: this.generatorWatts,
+            position: this.position
+            };
+            let statusCheck = {
+            completed: true,
+            roverStatus: roverStatus
+            };
+           response.results.push(statusCheck);
+          };
+        }
+
+        for (let m in commandBlockTest) {
+          if (commandBlockTest[m] === 'MODE_CHANGE') {
+           for (let n in commandBlockTest) {
+             if (commandBlockTest[n] === 'LOW_POWER') {
+             this.mode = 'LOW_POWER';
+             modeChange = {
+                completed: true
+              };
+            } else if (commandBlockTest[n] === 'NORMAL') {
+              this.mode = 'NORMAL';
+              modeChange = {
+               completed: true
+             };
+            } else {
+             modeChange = {
+                completed: false
+              };
+           }
+           }
+            response.results.push(modeChange);
+         } 
+        }
+
+       for (let i in commandBlockTest) {
+         if (commandBlockTest[i] === 'MOVE') {
+            for (let j in commandBlockTest) {
+             if (this.mode === 'NORMAL') {
+                this.position = commandBlockTest[j];
+                moveChange = {
+                  completed: true
+           };
+           } else {
+            moveChange = {
+             completed: false
+           };
+           }
+           }
+         response.results.push(moveChange);
+         }
+       }
+     }
+       return response;
+   }
+};
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,68 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  it ("constructor sets position and default values for mode and generatorWatts", function(){
+    let rover = new Rover(98382);
+    expect(rover.mode).toEqual('NORMAL');
+    expect(rover.generatorWatts).toEqual(110);
+  });
 
-});
+  it ("response returned by receiveMessage contains name of message", function(){
+    let rover = new Rover(98382);
+    let command = new Command('STATUS_CHECK');
+    let message = new Message('Status Check', command);
+    let response = rover.receiveMessage(message).message;
+    expect(response).toContain('Status Check');
+  });
+
+  it("response returned by recieveMessage includes two results if two commands are sent to the message", function(){
+    let rover = new Rover(98382);
+    let commands = [
+      new Command('MOVE', 4321),
+      new Command('STATUS_CHECK')
+    ];
+    let message = new Message('Test message with two commands', commands)
+    let response = rover.receiveMessage(message).message;
+    expect(response).toContain('Test message with two commands');
+  });
+
+  it("responds correctly to status check command", function(){
+    let rover = new Rover(98382);
+    let command = new Command('STATUS_CHECK');
+    let message = new Message('Status Check', command);
+    let response = rover.receiveMessage(message);
+    expect(response).toEqual({
+      message: 'Status Check',
+      results: [{completed: true, roverStatus: { mode: 'NORMAL', generatorWatts: 110, position: 98382 }}]
+      });
+  });
+
+  it("responds correctly to mode change command", function() {
+    let rover = new Rover(98382);
+    let command = new Command('MODE_CHANGE','LOW_POWER');
+    let message = new Message('Mode Change', command);
+    let response = rover.receiveMessage(message);
+    expect(response).toEqual({
+      message: 'Mode Change', 
+      results: [ { completed: true } ]});
+  });
+
+  it("responds with false completed value when attempting to move in LOW_POWER mode", function () {
+    let rover = new Rover(98382);
+    let command = new Command('MOVE');
+    let message = new Message('Move', command);
+    rover.mode = 'LOW_POWER';
+    let response = rover.receiveMessage(message).results;
+    expect(response).toContain(({completed: false}));
+  });
+
+  it("responds with position for move command", function() {
+    let rover = new Rover(98382);
+    let command = new Command('MOVE',10000);
+    let message = new Message('Move', command);
+    rover.mode = 'NORMAL';
+    let response = rover.receiveMessage(message);
+    expect(rover.position).toBe(10000);
+  })
+
+})
